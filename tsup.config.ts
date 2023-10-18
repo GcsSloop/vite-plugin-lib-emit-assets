bfb0eab0f649b404447aecf9f4b50c509f22ada4
GcsSloop <GcsSloop@gmail.com>
2023-10-18 20:25:15 +0800

    feat: 添加编译配置

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import type { Options } from 'tsup'
+
+export default <Options>{
+  entryPoints: [
+    'src/*.ts',
+  ],
+  clean: true,
+  format: ['cjs', 'esm'],
+  dts: true,
+  shims: true,
+  onSuccess: 'npm run build:fix',
+  external: ['vite', 'rollup'],
+}
